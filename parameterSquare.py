e7c841b3d45bb02b9aa3d675574385964b53eb7c
cmaceachern <caelmac12@gmail.com>
2019-09-11 10:05:17 -0400

    Added a file that prints hello

---
+++ parameterSquare.py
@@ -0,0 +1,12 @@
+# sqrParameter = float(input('Input one side length here: '))
+# print(sqrParameter, 'is your first length')
+#
+# sqrParameter2 = float(input('Input the second side length here: '))
+# print(sqrParameter2, "is your second length")
+#
+# sqrParameterFinal = 2*sqrParameter + 2*sqrParameter2
+# print(sqrParameterFinal, "is your final parameter")
+
+
+nameFull = input('Input your name here: ')
+print(nameFull)
